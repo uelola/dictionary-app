7ee11406916a7850c7491d3979ab7590c83d768c
uelola <80069286+uelola@users.noreply.github.com>
2021-05-09 22:19:29 +0200

    added minor styling for Results and changed h2->h5 for meaning.partOfSpeech

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,14 +4,14 @@ export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
+      <h5>{props.meaning.partOfSpeech}</h5>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
               {definition.definition}
               <br />
-              <em>{definition.example}</em>
+              <em>example: {definition.example}</em>
             </p>
           </div>
         );
